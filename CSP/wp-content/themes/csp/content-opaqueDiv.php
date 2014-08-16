21519b7e272d9763084ff6b58b7e9155bff80cc5
gauripadbidri <gpadbidri@genius-minds.com>
2014-08-16 14:58:38 +0530

    Modified the appearence of the Opaque DIV

--- CSP/wp-content/themes/csp/content-opaqueDiv.php
+++ CSP/wp-content/themes/csp/content-opaqueDiv.php
@@ -10,16 +10,18 @@
 
 <div id="open_close" class="col-md-4 col-lg-4 col-sm-4 col-xm-4 visible-lg visible-md hidden-sm hidden-xs">
     <span class="close-container">
-      <p style="color:black;">CSP Networks</p>
+      <p style="color:white; font-weight:bold;">CSP Networks</p>
     </span>
     <div class="open-container">
-        <span class="testimonial"><span id="playicon"></span>
-          <a href="#"><span style="color:black !important;">Play Testimonial</span>
-          </a>
-        </span>
-        <span class="case_study">
+      <div class="testimonial"><span class="testimonial glyphicon glyphicon-play-circle"></span>
           <a href="#">
-            <p style="color:black;"> Case Study </p>
-          </a> </span>
+            <span style="color:white !important;">Play Testimonial</span>
+          </a>
+      </div>
+      <div class="case_study"><span class="case_study glyphicon glyphicon-align-justify"></span>
+        <a href="#">
+          <span style="color:white !important;">Case Study</span>
+        </a>
+      </div>
     </div>     
 </div>
