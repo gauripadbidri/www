955cce47cda53c43bbc86d935a3bc9ba1a130002
gauripadbidri <gpadbidri@genius-minds.com>
2014-08-16 15:03:44 +0530

    Removed extra CSS Class which caused IFrame to load twice

--- CSP/wp-content/themes/csp/content-opaqueDiv.php
+++ CSP/wp-content/themes/csp/content-opaqueDiv.php
@@ -13,14 +13,14 @@
       <p style="color:white; font-weight:bold;">CSP Networks</p>
     </span>
     <div class="open-container">
-      <div class="testimonial"><span class="testimonial glyphicon glyphicon-play-circle"></span>
+      <div class="testimonial"><span class="glyphicon glyphicon-play-circle"></span>
           <a href="#">
-            <span style="color:white !important;">Play Testimonial</span>
+            <span style="color:white !important;"> Play Testimonial</span>
           </a>
       </div>
-      <div class="case_study"><span class="case_study glyphicon glyphicon-align-justify"></span>
+      <div class="case_study"><span class="glyphicon glyphicon-align-justify"></span>
         <a href="#">
-          <span style="color:white !important;">Case Study</span>
+          <span style="color:white !important;"> Case Study</span>
         </a>
       </div>
     </div>     
