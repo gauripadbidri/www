614814f4d7f0175b22ef3f62dec2487b82346c68
gauripadbidri <gpadbidri@genius-minds.com>
2014-08-18 21:34:11 +0530

    Modified Footer

--- CSP/wp-content/themes/csp/footer.php
+++ CSP/wp-content/themes/csp/footer.php
@@ -1,6 +1,10 @@
 </div>
-<footer class="container">
-	<div class="menuPosition">&copy; CSP Networks. All Rights Reserved.</div>
+<footer class="container col-md-offset-1">
+	<ul class="list-unstyled">
+		<li class="pull-right menuPosition">&copy; CSP Networks. All Rights Reserved.</li>
+		<li><a href="#">Privacy</a></li>
+	    <li><a href="#">Terms</a></li>
+	</ul>
 </footer>
 <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/jquery-ui.min.js"></script>
 <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri(); ?>/bootstrap/js/bootstrap.min.js"></script>
