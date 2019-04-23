548a9d13a7caf672d179a70a5e2a5f43a871a439
Plug in Git <gitbot@plugingit.com>
2019-04-23 04:02:00 +0000

    Merge current Shopify theme changes

---
+++ assets/custom.js
@@ -0,0 +1,12 @@
+//   $('.Collapsible__Button').click(function() {
+// let buttonID = this.id
+
+// function trial () {
+//     if ($('.kit-product-image').attr('data-item') == buttonID) {
+//         console.log(this);
+//         this.show();
+//         }
+// }
+
+//     trial ();
+//   });
